59dce247b3dbc4c548cd31713f1b6bb47f0d0a99
Maxim <fed4enko2018@yandex.ru>
2024-11-05 16:10:02 +0300

    Add middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uvicorn
 from api_v1 import router
 from fastapi.responses import HTMLResponse
 from fastui import prebuilt_html
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(router, tags=["API"])
@@ -10,6 +11,14 @@ app.include_router(router, tags=["API"])
 @app.get("/")
 async def root() -> HTMLResponse:
     return HTMLResponse(prebuilt_html(title="Тестовое задание"))
+
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+)
 
 
 if __name__ == "__main__":
